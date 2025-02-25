878db177dd51d5b3ada42852a601fa2213c4e808
Dil Bahadur Tharu <dtharu486@gmail.com>
2025-02-25 15:36:17 +0545

    chat app completed

---
+++ src/app.js
@@ -0,0 +1,152 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import { rateLimit } from "express-rate-limit";
+import session from "express-session";
+import fs from "fs";
+import { createServer } from "http";
+import passport from "passport";
+import path from "path";
+import requestIp from "request-ip";
+import { Server } from "socket.io";
+import swaggerUi from "swagger-ui-express";
+import { fileURLToPath } from "url";
+import YAML from "yaml";
+import { DB_NAME } from "./constants.js";
+import { dbInstance } from "./db/index.js";
+import morganMiddleware from "./logger/morgan.logger.js";
+import { initializeSocketIO } from "./socket/index.js";
+import { ApiError } from "./utils/ApiError.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = fs.readFileSync(path.resolve(__dirname, "./swagger.yaml"), "utf8");
+const swaggerDocument = YAML.parse(
+  file?.replace(
+    "- url: ${{server}}",
+    `- url: ${process.env.FREEAPI_HOST_URL || "http://localhost:8080"}/api/v1`
+  )
+);
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
+
+app.set("io", io);
+
+app.use(
+  cors({
+    origin:
+      process.env.CORS_ORIGIN === "*"
+        ? "*"
+        : process.env.CORS_ORIGIN?.split(","),
+    credentials: true,
+  })
+);
+
+app.use(requestIp.mw());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5000,
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req, res) => {
+    return req.clientIp;
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
+app.use(limiter);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(morganMiddleware);
+
+import userRouter from "./routes/apps/auth/user.routes.js";
+
+import chatRouter from "./routes/apps/chat-app/chat.routes.js";
+import messageRouter from "./routes/apps/chat-app/message.routes.js";
+
+import logger from "./logger/winston.logger.js";
+import { avoidInProduction } from "./middlewares/auth.middlewares.js";
+
+app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/chat-app/chats", chatRouter);
+app.use("/api/v1/chat-app/messages", messageRouter);
+
+initializeSocketIO(io);
+
+app.delete("/api/v1/reset-db", avoidInProduction, async (req, res) => {
+  if (dbInstance) {
+    await dbInstance.connection.db.dropDatabase({
+      dbName: DB_NAME,
+    });
+
+    const directory = "./public/images";
+
+    fs.readdir(directory, (err, files) => {
+      if (err) {
+        logger.error("Error while removing the images: ", err);
+      } else {
+        for (const file of files) {
+          if (file === ".gitkeep") continue;
+          fs.unlink(path.join(directory, file), (err) => {
+            if (err) throw err;
+          });
+        }
+      }
+    });
+    fs.unlink("./public/temp/seed-credentials.json", (err) => {
+      if (err) logger.error("Seed credentials are missing.");
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "Database dropped successfully"));
+  }
+  throw new ApiError(500, "Something went wrong while dropping the database");
+});
+
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      docExpansion: "none",
+    },
+    customSiteTitle: "FreeAPI docs",
+  })
+);
+
+export { httpServer };
